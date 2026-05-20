caf6c7db14160c75264fe54abeed5521469108fe
Varun Chadha <vichadha@protonmail.com>
2026-05-19 22:44:50 -0700

    Change text to white on black

--- Source/main.lua
+++ Source/main.lua
@@ -1,5 +1,9 @@
 import "CoreLibs/graphics"
 
+local gfx = playdate.graphics
+
 function playdate.update()
-  playdate.graphics.drawText("Hello, Marshall", 10, 10)
+  gfx.clear(gfx.kColorBlack)
+  gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
+  gfx.drawText("Hello, Marshall", 10, 10)
 end
