59b48d71d3bcb7d274244c0148064703f859abf1
Varun Chadha <vichadha@protonmail.com>
2026-05-20 13:57:48 -0700

    Center text

--- source/states/GameOverState.lua
+++ source/states/GameOverState.lua
@@ -8,8 +8,11 @@ local gfx <const> = playdate.graphics
 
 class('GameOverState').extends(GameState)
 function GameOverState:update()
+  local display_text = self.className
+  local text_w, text_h = gfx.getTextSize(display_text)
+
   gfx.clear()
-  gfx.drawText(self.className, CONSTANTS.SCREEN_W_HALF, CONSTANTS.SCREEN_H_HALF, kTextAlignment.center)
+  gfx.drawText(display_text, CONSTANTS.SCREEN_W_HALF - text_w / 2, CONSTANTS.SCREEN_H_HALF - text_h / 2, kTextAlignment.center)
 
   if (playdate.buttonJustPressed(playdate.kButtonA)) then
     setState(STATE_TITLE)
