0cd88024e997ff4fc058c6f7214df454fb8a3f50
Varun Chadha <vichadha@protonmail.com>
2026-06-22 11:10:05 -0700

    Improve error message for clamp

--- source/utilities/math.lua
+++ source/utilities/math.lua
@@ -1,7 +1,7 @@
 -- Lua and the Playdate SDK have no built-in clamp
 function math.clamp(value, min, max)
   if min > max then
-    error(string.format("clamp: min (%s) must be less than max (%s)", min, max), 2)
+    error(string.format("Error - clamp: min (%s) must be less than max (%s)", min, max), 2)
   end
 
   if (value < min) then return min end
